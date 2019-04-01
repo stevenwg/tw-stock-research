a277e42770d2429afcf220fd4e811b1c04f4a479
Luffy <luchaochuan@gmail.com>
2019-04-01 18:09:07 +0800

    nothing

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+#coding=utf-8
+
 ### TEST PyQt5 ###
 # import sys
 # from PyQt5 import QtCore, QtGui, QtWidgets
@@ -14,21 +16,31 @@
 ### TEST twstock ###
 import twstock
 # print(twstock.codes)
-import matplotlib.pyplot as plt
+from pyplotz.pyplotz import PyplotZ
+from pyplotz.pyplotz import plt
+# import matplotlib.pyplot as plt
 import pandas as pd
 
-stock_6207 = twstock.Stock('6207')
-stock_6207_2018 = stock_6207.fetch_from(2018,1)     # 獲取 2018 年 01 月至今日之股票資料
-stock_6207_2018_pd = pd.DataFrame(stock_6207_2018)
-stock_6207_2018_pd = stock_6207_2018_pd.set_index('date')
+stock_2317 = twstock.Stock('2317')
+stock_2317_2019 = stock_2317.fetch_from(2019,3)     # 獲取 2019 年 01 月至今日之股票資料
+stock_2317_2019_pd = pd.DataFrame(stock_2317_2019)
+stock_2317_2019_pd = stock_2317_2019_pd.set_index('date')
 
+stock_4938 = twstock.Stock('4938')
+stock_4938_2019 = stock_4938.fetch_from(2019,3)     # 獲取 2019 年 01 月至今日之股票資料
+stock_4938_2019_pd = pd.DataFrame(stock_4938_2019)
+stock_4938_2019_pd = stock_4938_2019_pd.set_index('date')
+
+pltz = PyplotZ() # create an instance
+pltz.enable_chinese()
 fig = plt.figure(figsize=(10, 6))
-plt.plot(stock_6207_2018_pd.close, '-' , label="收盤價")
-plt.plot(stock_6207_2018_pd.open, '-' , label="開盤價")
-plt.title('雷科股份2018 開盤/收盤價曲線',loc='right')
+pltz.plot(stock_2317_2019_pd.close, '-' , label='鴻海')
+pltz.plot(stock_4938_2019_pd.close, '-' , label='和碩')
+# pltz.plot(stock_2317_2019_pd.open, '-' , label='開盤價')
+pltz.title('收盤價曲線',loc='right')
 # loc->title的位置
-plt.xlabel('日期')
-plt.ylabel('收盤價')
+pltz.xlabel('日期')
+pltz.ylabel('收盤價')
 plt.grid(True, axis='y')
-plt.legend()
-fig.savefig('day20_01.png')
+pltz.legend()
+fig.savefig('test.png')
